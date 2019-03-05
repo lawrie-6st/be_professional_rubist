d01147ac88308956cae4c2f059090b93cb816eb5
hirano-wataru <42793984+hirano-wataru@users.noreply.github.com>
2019-03-05 16:37:24 +0900

    no message

--- Chapter_2/2_1_2_fizz_buzz.rb
+++ Chapter_2/2_1_2_fizz_buzz.rb
@@ -3,22 +3,12 @@
 # 3で割り切れる数であれば　fizz
 # 5で割り切れる数であれば_buzz
 # 15で割り切れる数であれば fizz_buzz
-
-account = Fizz_buzz.new
-class Fizz_buzz
-  def printoutput(number = nil)
-    answer1 = (number / 15).to_f
-    answer2 = (number / 15)
-    if  answer1 - answer2 == 0
-      p "fizz_buzz"
-    else
-      answer1 = (number / 3).to_f
-      answer2 = (number / 3)
-      p "fizz" if  answer1 - answer2 == 0
-      answer1 = (number / 5).to_f
-      answer2 = (number / 5)
-      p "buzz" if  answer1 - answer2 == 0
-    end
-  end
-  end
+# ruby Chapter_2/2_1_2_fizz_buzz.rb
+number = 30
+if (number % 3).to_f == 0 && (number % 5).to_f == 0
+  print 'fizz_buzz'
+else
+  print 'fizz' if (number % 3).to_f == 0
+  print 'buzz' if (number % 5).to_f == 0
 end
+
