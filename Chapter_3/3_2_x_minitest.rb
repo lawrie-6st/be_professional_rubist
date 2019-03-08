7c0c760c2d9b70551d8990926123b7b4ef456e86
hirano-wataru <42793984+hirano-wataru@users.noreply.github.com>
2019-03-08 14:49:26 +0900

    fix test env

--- Chapter_3/3_2_x_minitest.rb
+++ Chapter_3/3_2_x_minitest.rb
@@ -1,9 +1,8 @@
-# frozen_string_literal: true
 # ruby Chapter_3/3_2_x_minitest.rb
-require '/minitest/autorun'
-require 'minitest/assertions'
-class SampleTest < Minitest::MiniTest
-  def test_sample
-    assert_equal 'RUBY', 'ruby'.upcase
+require 'minitest/autorun'
+
+class SampleTest < Minitest::Test
+  def sample_test
+    assert_equal 'foo', 'foo'
   end
 end
