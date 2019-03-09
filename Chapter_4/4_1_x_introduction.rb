f2e26fa0ae4526b95c77bad4e20019768d4eb38a
hirano-wataru <42793984+hirano-wataru@users.noreply.github.com>
2019-03-09 12:18:41 +0900

    p87_4.2_array

---
+++ Chapter_4/4_1_x_introduction.rb
@@ -0,0 +1,7 @@
+# ruby Chapter_4/4.1.x_introduction.rb
+puts a = to_hex(0, 0, 0)
+puts to_hex(255, 255, 255)
+puts to_hex(4, 60, 120)
+puts to_ints('#000000')
+puts to_ints('#ffffff')
+puts to_ints('#043c78')
